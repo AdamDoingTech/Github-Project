4b16991a814556e3da5e7cd48d8b7f1e7cfc62f9
AdamDoingTech <olmpy@hotmail.com>
2023-04-01 00:54:22 -0600

    Initial Commit

---
+++ Github Project/Github_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ProjectApp.swift
+//  Github Project
+//
+//  Created by Adam on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
